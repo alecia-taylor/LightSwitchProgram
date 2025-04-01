77732b4794857ade67a2005f5f0b86efe388ad00
A. Taylor <alecia.taylor0@gmail.com>
2025-04-01 15:57:11 +0000

    Add initial script with day and light status checks

---
+++ script.js
@@ -0,0 +1,11 @@
+const isDay = false;
+console.log(isDay);
+
+const isLightOn = !(isDay);
+console.log("Daytime?");
+
+console.log(isDay);
+console.log("Lights on?");
+console.log(isLightOn);
+
+const batteryLevel = 50;
